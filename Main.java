a29130c69429adadf2710d9b179a489daf649388
derezzzed <64360794+derezzzed@users.noreply.github.com>
2020-05-30 13:33:00 +0300

    Update and rename LevelClass.java to Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class LevelClass {
+public class Main {
     public static void main(String[] args) {
 
         try {
